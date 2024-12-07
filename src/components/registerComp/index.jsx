a6894e885f214bf5ab43e1a95f21e59ecb6eea86
Muhammadrahimw <muqalla21@gmail.com>
2024-12-07 14:01:36 +0500

    code

--- src/components/registerComp/index.jsx
+++ src/components/registerComp/index.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 function RegisterComp() {
+	const postData = async () => {
+		const url = "https://git.heroku.com/authorizationw.git";
+		const data = {
+			name: "John Doe",
+			email: "johndoe@example.com",
+		};
+
+		try {
+			const response = await fetch(url, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			});
+
+			if (response.ok) {
+				const result = await response.json();
+				console.log("Success:", result);
+			} else {
+				console.error("Error:", response.status);
+			}
+		} catch (error) {
+			console.error("Request failed", error);
+		}
+	};
+
+	useEffect(() => {
+		postData();
+	}, []);
+
 	return <div>RegisterComp</div>;
 }
 
